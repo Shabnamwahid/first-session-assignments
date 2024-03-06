2568bfbfb43346d058a39bd1181514e527524954
Shabnamwahid <162483091+Shabnamwahid@users.noreply.github.com>
2024-03-06 22:19:20 +0500

    these are my first 3 files

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+// console.log("Hello World");
+// console.log("welcome to my channel");
+// console.log("1st Bite");
+//variables are like containers that stores value, or datatype.
+//let
+//var
+//const
+// let massage="hello world"
+// console.log(massage);
+// let massage="helo world"
+// massage="1st Bite";
+// console.log(massage);
+// var massage="it class"
+// console.log(massage);
+const massage = "dew";
+console.log(massage);
